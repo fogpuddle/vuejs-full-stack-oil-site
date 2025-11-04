14a7f5f6b7f3db872db2b6d484639d43fe0582c5
David Walters <4522382+fogpuddle@users.noreply.github.com>
2025-11-04 23:44:28 +0000

    firebase rearrangement

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,12 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import './main.css';
-import * as VueRouter from 'vue-router'
-import ShoppingCartPage from './pages/ShoppingCartPage.vue'
-import ProductsPage from './pages/ProductsPage.vue'
-import ProductDetailPage from './pages/ProductDetailPage.vue'
+import * as VueRouter from 'vue-router';
+import ShoppingCartPage from './pages/ShoppingCartPage.vue';
+import ProductsPage from './pages/ProductsPage.vue';
+import ProductDetailPage from './pages/ProductDetailPage.vue';
 import NotFoundPage from './pages/NotFoundPage.vue';
-import 'dotenv/config';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -15,12 +14,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
